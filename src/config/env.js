bc5966c756acc3c75b65c0dad96cf0a7eedc0387
Marc <marc.coder007@gmail.com>
2026-06-01 10:41:21 +0100

    Fix: adaptation des variables d'environnement pour Vercel

--- src/config/env.js
+++ src/config/env.js
@@ -1,6 +1,9 @@
 import { config } from "dotenv";
 
-config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
+// On ne cherche le fichier local QUE si on n'est pas sur Vercel
+if (!process.env.VERCEL) {
+  config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
+}
 
 export const { NODE_ENV, DB_URI, PORT, JWT_SECRET, JWT_EXPIRES_IN } =
   process.env;
